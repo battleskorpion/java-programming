cf830c83f40ae535803498fd8f4cca6f50699a59
BattleSkorpion <52262943+BattleSkorpion@users.noreply.github.com>
2021-03-23 19:21:31 -0400

    I hate logic

--- BusProject/src/bus_project/BusCalculation.java
+++ BusProject/src/bus_project/BusCalculation.java
@@ -3,6 +3,8 @@ package bus_project;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 public class BusCalculation {
 	private static ArrayList<LocalDate> dates; 
 	private static ArrayList<Integer> busesPerDate;					// number of buses used per each date  
@@ -19,18 +21,28 @@ public class BusCalculation {
 	// schedule customer on date selected
 	public static void scheduleTrip (Customer cstmr) {
 		LocalDate date = cstmr.getDate(); 
+		int merge; 		// whether or not to merge	// 0 = yes, 1 = no
 		
 		// if another booking from any customer exists on same date
 		if (dates.contains(date)) {
-			//// if another booking from identical customer exists on same date
+			//
 			for (int i = 0; i < customers.get(dates.indexOf(date)).size(); i++) {
-				if ()
-				
-				
-				
-				//// ask to merge customers? 
-				//JOptionPane.showMessageDialog(null, "There already exists a customer ")
-				//// if not merge
+				// if another booking from identical customer exists on same date
+				if (customers.get(dates.indexOf(date)).get(i).getName() == cstmr.getName()) {
+					// ask to merge customers? 
+					merge = JOptionPane.showConfirmDialog(null, "A customer with the same name was found on the same date. Merge the customers?", "Merge customers?" , JOptionPane.YES_NO_OPTION); 
+					
+					// if merge
+					if (merge == 0) {
+						
+					}
+					// if not merge
+					else {
+						
+					}
+					break; 
+				}
+
 				
 			}
 			// another booking from another customer(s) exists on same date
