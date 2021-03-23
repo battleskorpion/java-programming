a5f90ab63999231210a750ab5175bc73c11fca7a
BattleSkorpion <52262943+BattleSkorpion@users.noreply.github.com>
2021-03-23 14:47:38 -0400

    Create Bus.java

---
+++ BusProject/src/bus_project/Bus.java
@@ -0,0 +1,78 @@
+package bus_project;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Bus {
+	// constants
+	public static final int max_capacity = 20; 
+	public static final int min_capacity = 10;
+	public static final int max_buses = 20; 
+	private static ArrayList<LocalDate> dates; 
+	private static ArrayList<Integer> busesPerDate;		// number of buses used per each date  
+	
+	// instance variables
+	private int pax;			// number of passengers currently scheduled to take this bus
+	private LocalDate date; 	// date this bus is scheduled for 
+	
+	/* constructors */
+	
+	public Bus () {
+		date = LocalDate.now(); 
+		pax = 0; 
+	}
+	
+	/* precondition: dt before today */ 
+	public Bus (LocalDate dt) 
+	{
+		date = dt; 
+	}
+	
+	/* precondition: px <= max capacity */ 
+	public Bus (int px) {
+		pax = px; 
+	}
+	
+	/* precondition: dt before today, px <= max capacity */ 
+	public Bus (LocalDate dt, int px) 
+	{
+		date = dt; 
+		pax = px;
+	}
+	
+	/* method section */ 
+	
+	// returns number of passengers
+	public int getPax() 
+	{
+		return pax; 
+	}
+	
+	// returns date bus is scheduled for
+	public LocalDate getDate() 
+	{
+		return date; 
+	}
+	
+	public void setPax(int px) 
+	{
+		pax = px; 
+	}
+	
+	public void setDate (LocalDate dt) 
+	{
+		date = dt; 
+	}
+	
+	/* precondition: pax + px < max_capacity */ 
+	public void addPax(int px)
+	{
+		pax += px; 
+	}
+	
+	/* precondition: pax - px >= 0 */ 
+	public void removePax (int px) 
+	{
+		pax -= px; 
+	}
+}
