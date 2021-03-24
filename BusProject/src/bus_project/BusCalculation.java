f2804397726da0f569de40dc40df0a7810970f5c
BattleSkorpion <52262943+BattleSkorpion@users.noreply.github.com>
2021-03-24 09:11:55 -0400

    ja

--- BusProject/src/bus_project/BusCalculation.java
+++ BusProject/src/bus_project/BusCalculation.java
@@ -5,55 +5,115 @@ import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 
-public class BusCalculation {
+public class BusCalculation 
+{
+	// constants
+	public static final int MAX_BUSES = 20; 						// maximum buses available per day
+	public static final int MAX_CAPACITY = 20; 						// maximum capacity of a bus
+	public static final int MIN_CAPACITY = 10;						// minimum capacity a bus should be operating with
+	
+	// combined constants 
+	public static final int MAX_PAX = MAX_BUSES * MAX_CAPACITY; 	// maximum passengers per day
+	
+	// static variables
 	private static ArrayList<LocalDate> dates; 
-	private static ArrayList<Integer> busesPerDate;					// number of buses used per each date  
+	//private static ArrayList<Integer> busesPerDate;					// number of buses used per each date  
 	private static ArrayList<ArrayList<Customer>> customers; 	 	// customers each date 
 	
 	/* constructors */
 	
-	public BusCalculation () {
+	public BusCalculation () 
+	{
 		
 	}
 	
 	/* method section */ 
 	
 	// schedule customer on date selected
-	public static void scheduleTrip (Customer cstmr) {
+	public static int scheduleTrip (Customer cstmr) 
+	{
 		LocalDate date = cstmr.getDate(); 
-		int merge; 		// whether or not to merge	// 0 = yes, 1 = no
+		//int merge; 		// whether or not to merge	// 0 = yes, 1 = no
+		int dateIndex; 		// index of current date in dates array
+		int numPax; 		// number of other individual passengers (based on combined group size from all other groups that day) 
+		
 		
 		// if another booking from any customer exists on same date
-		if (dates.contains(date)) {
+		if (dates.contains(date)) 
+		{
 			//
-			for (int i = 0; i < customers.get(dates.indexOf(date)).size(); i++) {
-				// if another booking from identical customer exists on same date
-				if (customers.get(dates.indexOf(date)).get(i).getName() == cstmr.getName()) {
-					// ask to merge customers? 
-					merge = JOptionPane.showConfirmDialog(null, "A customer with the same name was found on the same date. Merge the customers?", "Merge customers?" , JOptionPane.YES_NO_OPTION); 
-					
-					// if merge
-					if (merge == 0) {
-						
-					}
-					// if not merge
-					else {
-						
-					}
-					break; 
-				}
-
-				
-			}
+			//for (int i = 0; i < customers.get(dates.indexOf(date)).size(); i++) {
+			//	// if another booking from identical customer exists on same date
+			//	if (customers.get(dates.indexOf(date)).get(i).getName() == cstmr.getName()) {
+			//		// ask to merge customers? 
+			//		merge = JOptionPane.showConfirmDialog(null, "A customer with the same name was found on the same date. Merge the customers?", "Merge customers?" , JOptionPane.YES_NO_OPTION); 
+			//		
+			//		// if merge
+			//		if (merge == 0) {
+			//			
+			//		}
+			//		// if not merge
+			//		else {
+			//			cstmr.setName(cstmr.getName() + "");
+			//		}
+			//		break; 
+			//	}
+            //
+			//	
+			//}
 			// another booking from another customer(s) exists on same date
 			//else {
-			//	customers.get(dates.indexOf(date)).add(cstmr); 
+				dateIndex = dates.indexOf(date); 
+				
+				// calculate numPax
+				numPax = getNumPaxOnDay(date); 
+				
+				if (numPax + cstmr.getNumPersons() <= MAX_PAX) {
+					customers.get(dateIndex).add(cstmr); 
+					
+					return 0; 	// no error
+				}
+				else {
+					return -2; 	// not enough buses 
+				}
 			//}
 			
 		}
 		// else (no booking on date)
-		else {	
-			
+		else 
+		{	
+			if (cstmr.getNumPersons() <= MAX_PAX) 
+			{
+				dates.add(cstmr.getDate()); 					// add new date 
+				customers.add(new ArrayList<Customer>());		// create new ArrayList aligning with new date
+				customers.get(customers.size() - 1).add(cstmr);	// add customer to new ArrayList
+			}
+			else 
+			{
+				return -2; 		// not enough buses 
+			}
+		}
+	}
+	
+	// calculates number of buses which will be necessary for the day
+	public static int getNumBuses(Customer cstmr) {
+		return 0;
+	
+		
+	}
+	
+	public static int getNumPaxOnDay(LocalDate dt) 
+	{
+		int numPax = 0; 	// number of passengers on the day
+		
+		if (dates.contains(dt)) {
+			for (Customer cstmr : customers.get(dates.indexOf(dt))) {
+				numPax += cstmr.getNumPersons(); 
+			}
+			return numPax; 
+		}
+		else {
+			return 0; 	// date not found in booked dates so no bookings are on the date, means no pax on the date
 		}
 	}
 }
